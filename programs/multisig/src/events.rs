0514ec4873101ddabe1ca736626ddb117eb99aef
tiamo <vk.tiamo@gmail.com>
2022-08-17 01:58:30 +0300

    feat: added events

---
+++ programs/multisig/src/events.rs
@@ -0,0 +1,41 @@
+use crate::*;
+
+#[event]
+pub struct WalletCreatedEvent {
+    #[index]
+    pub multisig: Pubkey,
+    pub owners: Vec<Pubkey>,
+    pub threshold: u8,
+    pub timestamp: i64,
+}
+
+#[event]
+pub struct TransactionCreatedEvent {
+    #[index]
+    pub multisig: Pubkey,
+    #[index]
+    pub transaction: Pubkey,
+    pub proposer: Pubkey,
+    pub instructions: Vec<TxInstruction>,
+    pub timestamp: i64,
+}
+
+#[event]
+pub struct TransactionApprovedEvent {
+    #[index]
+    pub multisig: Pubkey,
+    #[index]
+    pub transaction: Pubkey,
+    pub owner: Pubkey,
+    pub timestamp: i64,
+}
+
+#[event]
+pub struct TransactionExecutedEvent {
+    #[index]
+    pub multisig: Pubkey,
+    #[index]
+    pub transaction: Pubkey,
+    pub executor: Pubkey,
+    pub timestamp: i64,
+}
