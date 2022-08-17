d21e3745487f76ae1068457482ddb8d191aa4ecd
tiamo <vk.tiamo@gmail.com>
2022-08-18 01:45:07 +0300

    chore: added tests

--- tests/multisig.ts
+++ tests/multisig.ts
@@ -1,171 +1,192 @@
-import * as anchor from '@project-serum/anchor'
+import { AnchorProvider, Program, Wallet, web3 } from '@project-serum/anchor'
 import { assert } from 'chai'
+import { MultisigClient } from '../packages/multisig/src'
+import { IDL } from '../packages/multisig/src/idl'
+
+const opts = AnchorProvider.defaultOptions()
+const provider = new AnchorProvider(
+  new web3.Connection('http://localhost:8899', opts.preflightCommitment),
+  new Wallet(web3.Keypair.generate()),
+  AnchorProvider.defaultOptions(),
+)
 
 describe('multisig', () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.getProvider())
+  const client = new MultisigClient({
+    program: new Program(IDL, MultisigClient.programId, provider),
+    wallet: provider.wallet,
+  })
 
-  const program = anchor.workspace.SerumMultisig
+  const ownerA = web3.Keypair.generate()
+  const ownerB = web3.Keypair.generate()
+  const ownerC = web3.Keypair.generate()
+  const ownerD = web3.Keypair.generate()
+  const owners = [client.wallet.publicKey, ownerA.publicKey, ownerB.publicKey, ownerC.publicKey]
 
-  it('Tests the multisig program', async () => {
-    const multisig = anchor.web3.Keypair.generate()
-    const [multisigSigner, nonce]
-      = await anchor.web3.PublicKey.findProgramAddress(
-        [multisig.publicKey.toBuffer()],
-        program.programId,
-      )
-    const multisigSize = 200 // Big enough.
+  let multisigKey: web3.PublicKey
 
-    const ownerA = anchor.web3.Keypair.generate()
-    const ownerB = anchor.web3.Keypair.generate()
-    const ownerC = anchor.web3.Keypair.generate()
-    const ownerD = anchor.web3.Keypair.generate()
-    const owners = [ownerA.publicKey, ownerB.publicKey, ownerC.publicKey]
-
-    const threshold = new anchor.BN(2)
-    await program.rpc.createMultisig(owners, threshold, nonce, {
-      accounts: {
-        multisig: multisig.publicKey,
-      },
-      instructions: [
-        await program.account.multisig.createInstruction(
-          multisig,
-          multisigSize,
-        ),
-      ],
-      signers: [multisig],
-    })
-
-    let multisigAccount = await program.account.multisig.fetch(
-      multisig.publicKey,
+  before(async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(client.wallet.publicKey, 10 * web3.LAMPORTS_PER_SOL),
     )
-    assert.strictEqual(multisigAccount.nonce, nonce)
-    assert.ok(multisigAccount.threshold.eq(new anchor.BN(2)))
-    assert.deepStrictEqual(multisigAccount.owners, owners)
-    assert.ok(multisigAccount.ownerSetSeqno === 0)
+  })
 
-    const pid = program.programId
-    const accounts = [
-      {
-        pubkey: multisig.publicKey,
-        isWritable: true,
-        isSigner: false,
-      },
-      {
-        pubkey: multisigSigner,
-        isWritable: false,
-        isSigner: true,
-      },
-    ]
-    const newOwners = [ownerA.publicKey, ownerB.publicKey, ownerD.publicKey]
-    const data = program.coder.instruction.encode('set_owners', {
+  it('can create multisig', async () => {
+    const threshold = 2
+    const { transaction, key } = await client.createMultisig({ owners, threshold })
+    await provider.sendAndConfirm(transaction)
+
+    multisigKey = (await client.pda.multisig(key))[0]
+
+    const multisig = await client.fetchMultisig(multisigKey)
+    if (!multisig) {
+      throw new Error('Invalid multisig')
+    }
+
+    assert.equal(multisig.threshold, threshold)
+    assert.deepStrictEqual(multisig.owners, owners)
+    assert.ok(multisig.ownerSetSeqno === 0)
+  })
+
+  it('can create transaction', async () => {
+    const newOwners = [client.wallet.publicKey, ownerA.publicKey, ownerB.publicKey, ownerD.publicKey]
+    const data = client.program.coder.instruction.encode('set_owners', {
       owners: newOwners,
     })
 
-    const transaction = anchor.web3.Keypair.generate()
-    const txSize = 1000 // Big enough, cuz I'm lazy.
-    await program.rpc.createTransaction(pid, accounts, data, {
-      accounts: {
-        multisig: multisig.publicKey,
-        transaction: transaction.publicKey,
-        proposer: ownerA.publicKey,
-      },
-      instructions: [
-        await program.account.transaction.createInstruction(
-          transaction,
-          txSize,
-        ),
+    const index = 0
+    const instruction = new web3.TransactionInstruction({
+      programId: client.program.programId,
+      keys: [
+        {
+          pubkey: multisigKey,
+          isWritable: true,
+          isSigner: true,
+        },
       ],
-      signers: [transaction, ownerA],
+      data,
+    } as any)
+
+    const { transaction } = await client.createTransaction({
+      multisig: multisigKey,
+      instructions: [instruction],
+      index,
     })
 
-    const txAccount = await program.account.transaction.fetch(
-      transaction.publicKey,
-    )
+    try {
+      await provider.sendAndConfirm(transaction)
+    } catch (e) {
+      console.log(e)
+      throw e
+    }
 
-    assert.ok(txAccount.programId.equals(pid))
-    assert.deepStrictEqual(txAccount.accounts, accounts)
-    assert.deepStrictEqual(txAccount.data, data)
-    assert.ok(txAccount.multisig.equals(multisig.publicKey))
-    assert.deepStrictEqual(txAccount.didExecute, false)
-    assert.ok(txAccount.ownerSetSeqno === 0)
+    const tx = await client.getTransaction(multisigKey, index)
+    if (!tx) {
+      throw new Error('Invalid transaction')
+    }
 
-    // Other owner approves transaction.
-    await program.rpc.approve({
-      accounts: {
-        multisig: multisig.publicKey,
-        transaction: transaction.publicKey,
-        owner: ownerB.publicKey,
-      },
-      signers: [ownerB],
-    })
-
-    // Now that we've reached the threshold, send the transaction.
-    await program.rpc.executeTransaction({
-      accounts: {
-        multisig: multisig.publicKey,
-        multisigSigner,
-        transaction: transaction.publicKey,
-      },
-      remainingAccounts: program.instruction.setOwners
-        .accounts({
-          multisig: multisig.publicKey,
-          multisigSigner,
-        })
-        // Change the signer status on the vendor signer since it's signed by the program, not the client.
-        .map(meta =>
-          meta.pubkey.equals(multisigSigner)
-            ? { ...meta, isSigner: false }
-            : meta,
-        )
-        .concat({
-          pubkey: program.programId,
-          isWritable: false,
-          isSigner: false,
-        }),
-    })
-
-    multisigAccount = await program.account.multisig.fetch(multisig.publicKey)
-
-    assert.strictEqual(multisigAccount.nonce, nonce)
-    assert.ok(multisigAccount.threshold.eq(new anchor.BN(2)))
-    assert.deepStrictEqual(multisigAccount.owners, newOwners)
-    assert.ok(multisigAccount.ownerSetSeqno === 1)
+    assert.equal(tx.index, 0)
+    assert.equal(tx.proposer.toBase58(), client.wallet.publicKey.toBase58())
+    assert.deepEqual(tx.instructions, [instruction])
+    assert.equal(tx.signers.length, owners.length)
+    assert.equal(tx.signers[0], true)
   })
 
-  it('Assert Unique Owners', async () => {
-    const multisig = anchor.web3.Keypair.generate()
-    const [, nonce]
-      = await anchor.web3.PublicKey.findProgramAddress(
-        [multisig.publicKey.toBuffer()],
-        program.programId,
-      )
-    const multisigSize = 200 // Big enough.
-
-    const ownerA = anchor.web3.Keypair.generate()
-    const ownerB = anchor.web3.Keypair.generate()
-    const owners = [ownerA.publicKey, ownerB.publicKey, ownerA.publicKey]
-
-    const threshold = new anchor.BN(2)
+  it('can not execute unapproved transaction', async () => {
+    const index = 0
+    const { transaction } = await client.executeTransaction({
+      multisig: multisigKey,
+      index,
+    })
     try {
-      await program.rpc.createMultisig(owners, threshold, nonce, {
-        accounts: {
-          multisig: multisig.publicKey,
-          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-        },
-        instructions: [
-          await program.account.multisig.createInstruction(
-            multisig,
-            multisigSize,
-          ),
-        ],
-        signers: [multisig],
-      })
-      assert.fail()
-    } catch (err) {
-      const error = err.error
-      assert.strictEqual(error.errorCode.number, 6008)
-      assert.strictEqual(error.errorMessage, 'Owners must be unique')
+      await provider.sendAndConfirm(transaction)
+      assert.ok(false)
+    } catch (e: any) {
+      assertErrorCode(e, 'NotEnoughSigners')
+    }
+  })
+
+  it('can approve transaction', async () => {
+    const { transaction } = await client.approveTransaction({
+      multisig: multisigKey,
+      owner: ownerA.publicKey,
+      index: 0,
+    })
+    try {
+      await provider.sendAndConfirm(transaction, [ownerA])
+    } catch (e: any) {
+      console.log(e)
+      throw e
+    }
+
+    const tx = await client.getTransaction(multisigKey, 0)
+    if (!tx) {
+      throw new Error('Invalid transaction')
+    }
+
+    assert.equal(tx.signers[1], true)
+  })
+
+  it('can execute approved transaction', async () => {
+    const index = 0
+
+    const { transaction } = await client.executeTransaction({
+      multisig: multisigKey,
+      index,
+    })
+
+    try {
+      await provider.sendAndConfirm(transaction)
+    } catch (e: any) {
+      console.log(e)
+    }
+
+    const tx = await client.getTransaction(multisigKey, index)
+    if (!tx) {
+      throw new Error('Invalid transaction')
+    }
+
+    assert.equal(tx.executor.toBase58(), client.wallet.publicKey.toBase58())
+    assert.ok(tx.executedAt !== null)
+
+    const multisig = await client.fetchMultisig(multisigKey)
+    if (!multisig) {
+      throw new Error('Invalid multisig')
+    }
+
+    const newOwners = [client.wallet.publicKey, ownerA.publicKey, ownerB.publicKey, ownerD.publicKey]
+    assert.deepStrictEqual(multisig.owners, newOwners)
+    assert.deepStrictEqual(multisig.ownerSetSeqno, 1)
+  })
+
+  it('can not change threshold off-chain', async () => {
+    const transaction = await client.program.methods
+      .changeThreshold(3)
+      .accounts({ multisig: multisigKey })
+      .transaction()
+
+    try {
+      await provider.sendAndConfirm(transaction)
+      assert.ok(false)
+    } catch (e: any) {
+      assert.ok(String(e).includes('Signature verification failed'))
+    }
+  })
+
+  it('can not set owners off-chain', async () => {
+    const transaction = await client.program.methods
+      .setOwners([ownerA.publicKey, ownerB.publicKey])
+      .accounts({ multisig: multisigKey })
+      .transaction()
+
+    try {
+      await provider.sendAndConfirm(transaction)
+      assert.ok(false)
+    } catch (e: any) {
+      assert.ok(String(e).includes('Signature verification failed'))
     }
   })
 })
+
+export function assertErrorCode(error: { logs?: string[] }, code: string) {
+  assert.ok(String((error?.logs ?? []).join('')).includes(`Error Code: ${code}`))
+}
