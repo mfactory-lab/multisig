9e6e1c5203bd20bce42788e5e7bfb7201e9bf8fc
tiamo <vk.tiamo@gmail.com>
2022-08-18 03:32:48 +0300

    fix(cli): global improvements

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -21,7 +21,7 @@ cli
   .option(
     '-k, --keypair <file>',
     'Solana wallet location',
-    './target/deploy/multisig-keypair.json',
+    `${process.env.HOME}/.config/solana/id.json`,
   )
   .option('-l, --log-level <string>', 'log level', (l: any) => {
     l && log.setLevel(l)
@@ -40,21 +40,18 @@ const client = new MultisigClient({
   wallet: provider.wallet,
 })
 
-cli.command('show-multisig')
-  .argument('<key>', 'Multisig derived key')
-  .action(async (key: string) => {
-    const multisig = await client.getMultisig(key)
-    console.log(JSON.stringify(multisig))
-  })
-
 cli.command('create-multisig')
-  .requiredOption('-K, --keys <keys>', 'Owner keys separated by comma')
-  .requiredOption('-t, --threshold <number>', 'Multisig threshold')
-  .option('-k, --key <key>', 'Customize multisig derived key')
+  .description('Create new multisig')
+  .requiredOption('--keys <keys>', 'Owner keys (separated by comma)')
+  .requiredOption('-t, --threshold <number>', 'Minimum number of owner approvals needed to sign a transaction')
+  .option('-k, --key <base58>', 'Multisig derived key (default auto-generated)')
   .action(async (opts: any) => {
+    const owners = new Set(opts.keys.split(','))
+    owners.add(client.wallet.publicKey.toBase58())
+
     const { transaction, key } = await client.createMultisig({
-      owners: opts.keys.split(',').map((k: string) => new web3.PublicKey(k)),
-      threshold: opts.threshold,
+      owners: [...owners].map(k => new web3.PublicKey(k as string)),
+      threshold: Number(opts.threshold),
       key: opts.key ?? null,
     })
     try {
@@ -68,29 +65,40 @@ cli.command('create-multisig')
     }
   })
 
+cli.command('show-multisig')
+  .argument('<key>', 'Multisig derived key')
+  .action(async (key: string) => {
+    const multisig = await client.getMultisig(key)
+    console.log(JSON.stringify(multisig, null, 2))
+  })
+
+cli.command('show-owned-multisig')
+  .description('Show all owned multisig accounts')
+  .action(async () => {
+    const list = await client.findMultisigByOwner(client.wallet.publicKey)
+    log.info(JSON.stringify(list, null, 2))
+  })
+
+cli.command('delete-multisig')
+  .argument('<key>', 'Multisig derived key')
+  .action(() => {
+    log.info('Unimplemented')
+  })
+
 cli.command('create-transaction')
-  .requiredOption('-m, --multisig <string>', 'Multisig derived key')
-  .option('-k, --key <key>', 'Customize multisig derived key')
-  .action(async (opts: any) => {
+  .argument('<file>', 'Instructions file')
+  .requiredOption('-m, --multisig <key>', 'Multisig derived key')
+  .option('-i, --index <number>', 'Custom transaction index')
+  .action(async (file, opts: any) => {
     const [multisigKey] = await client.pda.multisig(opts.multisig)
 
-    // TODO:
-    const data = ''
-    const instruction = new web3.TransactionInstruction({
-      programId: client.program.programId,
-      keys: [
-        {
-          pubkey: multisigKey,
-          isWritable: true,
-          isSigner: true,
-        },
-      ],
-      data,
-    } as any)
+    const instructions = Array.from(JSON.parse(fs.readFileSync(opts.keypair).toString()))
+      .map((i: any) => new web3.TransactionInstruction(i))
 
     const { transaction, key } = await client.createTransaction({
       multisig: multisigKey,
-      instructions: [instruction],
+      instructions,
+      index: opts.index ?? null,
     })
 
     try {
@@ -104,9 +112,16 @@ cli.command('create-transaction')
     }
   })
 
+cli.command('delete-transaction')
+  .argument('<index>', 'Transaction index')
+  .requiredOption('-m, --multisig <key>', 'Multisig derived key')
+  .action(() => {
+    log.info('Unimplemented')
+  })
+
 cli.command('approve')
   .argument('<index>', 'Transaction index')
-  .requiredOption('-m, --multisig <string>', 'Multisig key')
+  .requiredOption('-m, --multisig <key>', 'Multisig key')
   .action(async (index: string, opts: any) => {
     const [multisigKey] = await client.pda.multisig(opts.multisig)
 
@@ -125,9 +140,42 @@ cli.command('approve')
     }
   })
 
+cli.command('approve-all')
+  .requiredOption('-m, --multisig <key>', 'Multisig key')
+  .action(async (opts: any) => {
+    const [multisig] = await client.pda.multisig(opts.multisig)
+    const transactions = await client.findTransactions({ multisig })
+
+    log.info(`Found ${transactions.length} transaction(s)...`)
+
+    let approved = 0
+    for (const { account } of transactions) {
+      if (!account.executedAt) {
+        log.info(`Approving transaction #${account.index}...`)
+        try {
+          const { transaction } = await client.approveTransaction({
+            multisig,
+            index: Number(account.index),
+          })
+          const sig = await provider.sendAndConfirm(transaction)
+          log.info(`Signature: ${sig}`)
+          log.info('OK')
+          approved++
+        } catch (e) {
+          log.info('Error')
+          console.log(e)
+        }
+      }
+    }
+
+    if (transactions.length > 0) {
+      log.info(`Total approved: ${approved}`)
+    }
+  })
+
 cli.command('execute')
   .argument('<index>', 'Transaction index')
-  .requiredOption('-m, --multisig <string>', 'Multisig key')
+  .requiredOption('-m, --multisig <key>', 'Multisig key')
   .action(async (index: string, opts: any) => {
     const [multisig] = await client.pda.multisig(opts.multisig)
 
